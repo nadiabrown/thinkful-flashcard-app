58e44a50e8d1cb8a6a4a3e3855e24b1a7387c8f4
Nadia <nadiavaleah@gmail.com>
2024-01-24 21:53:33 -0600

    Render decks for the home component.

---
+++ src/Layout/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+function Home() {
+  const dat
+  return (
+    <div>
+      <p>Home component renders!</p>
+      <button>Create Task</button>
+    </div>
+  );
+}
+
+export default Home;
